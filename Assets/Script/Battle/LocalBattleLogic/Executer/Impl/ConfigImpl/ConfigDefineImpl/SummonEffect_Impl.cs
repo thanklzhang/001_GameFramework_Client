487a15321adf985650fc2ef35f066739887fec5b
thanklzhang <940678686@qq.com>
2024-09-13 01:09:24 +0800

    【battle】召唤逻辑增加

---
+++ Assets/Script/Battle/LocalBattleLogic/Executer/Impl/ConfigImpl/ConfigDefineImpl/SummonEffect_Impl.cs
@@ -0,0 +1,79 @@
+/*
+ * generate by tool
+*/
+//using System.Collections;
+using System.Collections.Generic;
+using Config;
+namespace Battle
+{
+    
+    
+ 
+    public class SummonEffect_Impl : ISummonEffect
+    {
+        private Config.SummonEffect config;
+        
+        public void Init(int id)
+        {
+            config = ConfigManager.Instance.GetById<Config.SummonEffect>(id);
+        }
+        
+        public int Id => config.Id;
+        
+        /// <summary>
+        ///技能名称
+        /// </summary>
+        public string Name => config.Name;
+        
+        /// <summary>
+        ///介绍
+        /// </summary>
+        public string Describe => config.Describe;
+        
+        /// <summary>
+        ///召唤物实体配置id
+        /// </summary>
+        public int SummonConfigId => config.SummonConfigId;
+        
+        /// <summary>
+        ///召唤物出生点类型
+        /// </summary>
+        public int SummonBornPosType => config.SummonBornPosType;
+        
+        /// <summary>
+        ///召唤数量
+        /// </summary>
+        public int SummonCount => config.SummonCount;
+        
+        /// <summary>
+        ///最大召唤数量
+        /// </summary>
+        public int MaxSummonCount => config.MaxSummonCount;
+        
+        /// <summary>
+        ///召唤物持续时间(ms)
+        /// </summary>
+        public int LastTime => config.LastTime;
+        
+        /// <summary>
+        ///持续时间附加组(ms)
+        /// </summary>
+        public List<List<int>> LastTimeAddedGroup => config.LastTimeAddedGroup;
+        
+        /// <summary>
+        ///召唤物增加的属性组(,分割)
+        /// </summary>
+        public List<int> AddedAttrGroup => config.AddedAttrGroup;
+        
+        /// <summary>
+        ///增加属性组数值(,|分割 目前只做一个属性之只受一种属性增加)
+        /// </summary>
+        public List<List<int>> AddedValueGroup => config.AddedValueGroup;
+        
+        /// <summary>
+        ///效果资源id
+        /// </summary>
+        public int EffectResId => config.EffectResId;
+        
+    } 
+}
---
+++ Assets/Script/Config/ConfigDefine/SummonEffect.cs
@@ -0,0 +1,96 @@
+/*
+ * generate by tool
+*/
+//using System.Collections;
+using System.Collections.Generic;
+namespace Config
+{
+    
+    
+    
+       
+    public class SummonEffect : BaseConfig
+    {
+        
+        /// <summary>
+        ///技能名称
+        /// </summary>
+        private string name; 
+        
+        /// <summary>
+        ///介绍
+        /// </summary>
+        private string describe; 
+        
+        /// <summary>
+        ///召唤物实体配置id
+        /// </summary>
+        private int summonConfigId; 
+        
+        /// <summary>
+        ///召唤物出生点类型
+        /// </summary>
+        private int summonBornPosType; 
+        
+        /// <summary>
+        ///召唤数量
+        /// </summary>
+        private int summonCount; 
+        
+        /// <summary>
+        ///最大召唤数量
+        /// </summary>
+        private int maxSummonCount; 
+        
+        /// <summary>
+        ///召唤物持续时间(ms)
+        /// </summary>
+        private int lastTime; 
+        
+        /// <summary>
+        ///持续时间附加组(ms)
+        /// </summary>
+        private List<List<int>> lastTimeAddedGroup; 
+        
+        /// <summary>
+        ///召唤物增加的属性组(,分割)
+        /// </summary>
+        private List<int> addedAttrGroup; 
+        
+        /// <summary>
+        ///增加属性组数值(,|分割 目前只做一个属性之只受一种属性增加)
+        /// </summary>
+        private List<List<int>> addedValueGroup; 
+        
+        /// <summary>
+        ///效果资源id
+        /// </summary>
+        private int effectResId; 
+        
+
+        
+        public string Name { get => name; }     
+        
+        public string Describe { get => describe; }     
+        
+        public int SummonConfigId { get => summonConfigId; }     
+        
+        public int SummonBornPosType { get => summonBornPosType; }     
+        
+        public int SummonCount { get => summonCount; }     
+        
+        public int MaxSummonCount { get => maxSummonCount; }     
+        
+        public int LastTime { get => lastTime; }     
+        
+        public List<List<int>> LastTimeAddedGroup { get => lastTimeAddedGroup; }     
+        
+        public List<int> AddedAttrGroup { get => addedAttrGroup; }     
+        
+        public List<List<int>> AddedValueGroup { get => addedValueGroup; }     
+        
+        public int EffectResId { get => effectResId; }     
+        
+
+    } 
+}

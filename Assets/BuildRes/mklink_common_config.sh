5be1d9c8ccd9e80b4d9cf1fe52a15a1e9bbac09b
ZhangXiangYu-Zxy <zhangxiangyu@secretlisa.com>
2024-10-18 11:58:41 +0800

    change 增加 mac 的处理文件

---
+++ Assets/BuildRes/mklink_common_config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash  
+  
+
+  
+# 创建符号链接  
+ln -s -f ../../../001_GameFramework_Config/CommonData/Config Config  
+ln -s -f ../../../001_GameFramework_Config/CommonData/Plot Plot  
+ln -s -f ../../../001_GameFramework_Config/CommonData/Battle Battle  
+  
+# 暂停  
+echo "Press Enter to continue..."  
+read

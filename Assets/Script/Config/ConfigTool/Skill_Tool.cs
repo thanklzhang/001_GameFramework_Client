9562fcecc8d38e94cf264395c106df2c6b537852
ZhangXiangYu-Zxy <zhangxiangyu@secretlisa.com>
2025-04-28 19:46:05 +0800

    [battle]修改 ai 代码

--- Assets/Script/Config/ConfigTool/Skill_Tool.cs
+++ Assets/Script/Config/ConfigTool/Skill_Tool.cs
@@ -32,6 +32,8 @@ namespace Config
         // }
 
         //
+        
+        //获取释放时候产生关键效果的区域效果 id
         public static int GetKeyAreaEft(int skillId)
         {
             var skillConfig = BattleConfigManager.Instance.GetById<ISkill>(skillId);
@@ -46,6 +48,11 @@ namespace Config
                         if (type == SkillEffectType.AreaEffect)
                         {
                             return eftId;
+                        }
+                        else if (type == SkillEffectType.ConditionActionEffect)
+                        {
+                            //如果有影响释放目标单位等的条件判断 如释放经的时候生命值小于50%给自己套盾，否则释放周围区域伤害   这种目前暂定，
+                            //如需拓展 那么需要深度检测条件 目前没那必要 有必要再加
                         }
                     }
                 }
